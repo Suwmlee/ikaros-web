49debee6e0d1a180ee8496991a9f5ac25941ccc9
Mathhew <suwmlee@gmail.com>
2021-06-08 14:59:37 +0800

    Add menu

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,19 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <el-container>
       <el-header>
         <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-          <el-menu-item index="1">快速开始</el-menu-item>
-          <el-menu-item index="2">配置</el-menu-item>
+          <el-menu-item index="1">简介</el-menu-item>
           <el-menu-item index="3">刮削</el-menu-item>
           <el-menu-item index="4">转移</el-menu-item>
           <el-menu-item index="5">重命名</el-menu-item>
-          <el-menu-item index="6">计划任务</el-menu-item>
         </el-menu>
       </el-header>
       <el-main>
-        <Tutorial v-if="activeIndex==='1'" msg="Welcome to Ikaros"/>
-        <ScrapingConf v-if="activeIndex==='2'" />
-        <Scraping v-if="activeIndex==='3'" />
+        <Tutorial v-if="activeIndex==='1'" />
+        <JavScraping v-if="activeIndex==='3'" />
         <Transfer v-if="activeIndex==='4'" />
         <Reanme v-if="activeIndex==='5'" />
-        <Task v-if="activeIndex==='6'" />
       </el-main>
     </el-container>
 
@@ -27,21 +22,17 @@
 
 <script lang="ts">
 import Tutorial from './components/tutorial.vue'
-import Scraping from './components/scraping.vue'
-import ScrapingConf from './components/scrapingconf.vue'
+import JavScraping from './components/javscraping.vue'
 import Transfer from './components/transfer.vue'
 import Reanme from './components/rename.vue'
-import Task from './components/task.vue'
 
 export default {
   name: 'App',
   components: {
     Tutorial,
-    Scraping,
-    ScrapingConf,
+    JavScraping,
     Transfer,
     Reanme,
-    Task
   },
   data() {
     return {
@@ -61,7 +52,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
